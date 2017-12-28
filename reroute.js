2541b841cd8cd0d0334efa8351002d404c4b3c65
Rochelle Manongdo <rochellem@Rochelles-MacBook-Pro.local>
2017-12-27 22:15:28 -0800

    slowly eliminate the problem

--- reroute.js
+++ reroute.js
@@ -1,20 +1,20 @@
-var app = angular.module("appWebsite", ["ngRoute"]);
+// var app = angular.module("appWebsite", ["ngRoute"]);
 
-app.config(function($routeProvider){
-  $routeProvider
-  .when("/", {
-    templateUrl : "index.html"  
-  })  
-  .when("/Home", {
-    templateUrl : "index.html"
-  })
-  .when("/About", {
-    templateUrl : "about.html"
-  })
-  .when("/Experience", {
-    templateUrl : "experience.html"
-  })
-  .when("/Contact", {
-    templateUrl : "contact.html"
-  });
-});
+// app.config(function($routeProvider){
+//   $routeProvider
+//   .when("/", {
+//     templateUrl : "index.html"  
+//   })  
+//   .when("/Home", {
+//     templateUrl : "index.html"
+//   })
+//   .when("/About", {
+//     templateUrl : "about.html"
+//   })
+//   .when("/Experience", {
+//     templateUrl : "experience.html"
+//   })
+//   .when("/Contact", {
+//     templateUrl : "contact.html"
+//   });
+// });
